50779117c8bcbe04a98068dca22d4c34f2828973
TajwarSaiyeed <rjabid36@gmail.com>
2022-11-25 13:57:38 +0600

    AXIOS add and blogs fetch by axios

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Blogs = () => {
   const [qnas, setQNAS] = useState([]);
   useEffect(() => {
-    fetch("blogs.json")
-      .then((res) => res.json())
-      .then((data) => setQNAS(data));
+    // axios user for getting blogs data
+    axios
+      .get(`${process.env.REACT_APP_SERVER}/blogs`)
+      .then((data) => setQNAS(data.data));
   }, []);
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 my-5">
       {qnas?.map((qna) => {
         return (
-          <div key={qna.id} className="p-5 bg-base-300 rounded-md">
+          <div key={qna._id} className="p-5 bg-base-300 rounded-md">
             <h1 className="text-xl font-bold">{qna.question}</h1>
             <p className="py-6 text-justify">{qna.answer}</p>
           </div>
